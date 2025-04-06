4016cff6f43b3f998085ef5ccc75d7387a5e809e
rappekun <rappekun777@gmail.com>
2025-04-06 14:51:56 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/global.css";
+import type { Metadata } from "next";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
